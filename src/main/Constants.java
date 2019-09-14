c03e5a2d2dfd2762e6af3677a6ea05ef16dbc55d
maxim <laba1203@gmail.com>
2019-09-14 13:21:52 +0300

    small changes

--- src/main/Constants.java
+++ src/main/Constants.java
@@ -3,7 +3,7 @@ package main;
 public class Constants {
     public static int WIDTH = 800; //ширина
     public static int HEIGHT = 600; //высота
-    public static String NAME = "SPACE BATTLE"; //TODO
+    public static String NAME = "SPACE BATTLE";
 
     public enum Direction{ LEFT, RIGHT, NONE };
 
